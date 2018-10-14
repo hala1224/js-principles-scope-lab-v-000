8c14d564c00095aa73f9e7efa32fc2104a9179d3
Natalie Halawani <hala1224@gmail.com>
2018-10-14 16:56:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Write your solution in this file!
 var customerName='bob';
 function upperCaseCustomerName(customerName) {
-
   customerName=customerName.toUpperCase();
   return customerName;
 }
