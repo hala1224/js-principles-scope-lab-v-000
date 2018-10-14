6e821fb8dfedc7c679c755e4c2bb99493e70246b
Natalie Halawani <hala1224@gmail.com>
2018-10-14 16:52:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var customerName='bob';
 function upperCaseCustomerName(customerName) {
   console.log(customerName);
-  customerName.toUpperCase();
+  customerName=customerName.toUpperCase();
 }
 function setBestCustomer() {
   bestCustomer='not bob';
